b1c2c31303db6a261fdc32ce22fb4220128850bc
YnezArk <ynez_ark@163.com>
2025-12-29 15:06:37 +0800

    qt项目

---
+++ Java_Programming/PTA_Tests/src/PTAweek14/Average.java
@@ -0,0 +1,8 @@
+package PTAweek14;
+
+/**
+    Author: Ynez Ark
+    Date: 2025/12/21 16:13
+*/
+    public class Average {
+}
---
+++ Java_Programming/PTA_Tests/src/PTAweek14/Dictionary.java
@@ -0,0 +1,8 @@
+package PTAweek14;
+
+/**
+    Author: Ynez Ark
+    Date: 2025/12/21 16:18
+*/
+    public class Dictionary {
+}
