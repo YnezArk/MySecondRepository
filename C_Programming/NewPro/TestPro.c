78b3a6041bcac16c5e5ee531e9e9f551cc7c8099
YnezArk <ynez_ark@163.com>
2026-02-15 10:58:53 +0800

    存档解码脚本

---
+++ C_Programming/NewPro/TestPro.c
@@ -0,0 +1,20 @@
+/*
+    Author: Ynez Ark
+    Date: 2026/2/14 15:34
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+    system("chcp 65001 > nul");
+    
+    
+    printf("Hello, World!\n");
+    
+    
+    
+    printf("\n");
+    system("pause");   
+    return 0;
+}
